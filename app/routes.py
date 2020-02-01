24c7cf4af8228f5ec5f7814a121975acbcf9717f
MJM <mjmcgaffick@yahoo.com>
2020-02-01 02:24:59 -0500

    Implements post into input form and redirects you to correct output based on radio button selected when pushing run

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
-from flask import render_template
+from flask import  Flask, redirect, url_for, render_template, request
 from app import app
 
-@app.route('/')
+
+@app.route('/',methods=["GET", "POST"])
 def index():
+   if request.method == "POST":
+      stat_type = request.form['statistics']
+      if stat_type == "basic":
+         return redirect(url_for('basic'))
+      else:
+         return redirect(url_for('descriptive'))
+   else:
+      return render_template('input_form.html')
+
+@app.route('/basic', methods=["GET", "POST"])
+def basic():
    return render_template('output_basic_form.html')
+
+@app.route('/descriptive',methods=["GET", "POST"])
+def descriptive():
+   return render_template('output_descriptive_form.html')
