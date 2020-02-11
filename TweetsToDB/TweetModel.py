58a6313cb3f785d863c8791ab75250b3f6fd3664
Austin Vershave <austin.vershave@gmail.com>
2020-02-11 08:30:09 -0500

    fixed tweet model and fixed datatable

--- TweetsToDB/TweetModel.py
+++ TweetsToDB/TweetModel.py
@@ -1,8 +1,12 @@
 from mongoengine import *
 
 class Tweet(Document):
+    tweetID = IntField()
     tweetCreator = StringField()
     tweetText = StringField()
+    tweetLikes = IntField()
+    tweetRe = IntField()
     creatorFollowers = IntField()
     mentions = ListField()
+    location = StringField()
     dateCreated = DateTimeField()
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import  Flask, redirect, url_for, render_template, request, flash
+from flask import  Flask, redirect, url_for, render_template, request, jsonify
 from app import app
 from models.SavedPage import SavedPage
+from TweetsToDB.TweetModel import Tweet
 from mongoengine import *
+import pandas as pd
 
 
 @app.route('/',methods=["GET", "POST"])
@@ -10,9 +12,13 @@ def index():
 
 @app.route('/stats', methods=['POST'])
 def stats():
+   print(request.form)
    stat_type = request.form.get('statistics')
    filter_term = request.form.get('input_term')
-   newPage = SavedPage(pageType=stat_type, filterTerm=filter_term).save()
+   filter_time = request.form.get('input_time')
+   filter_location = request.form.get('input_location')
+   filter_profile = request.form.get('input_profile')
+   newPage = SavedPage(pageType=stat_type, filterTerm=filter_term, filterTime=filter_time, filterLocation=filter_location, filterProfile=filter_profile).save()
    if stat_type == "basic":
       return redirect((url_for('basic', _id=newPage.id)))
    else:
@@ -21,7 +27,9 @@ def stats():
 @app.route('/basic/<_id>', methods=["GET", "POST"])
 def basic(_id):
    reqPage = SavedPage.objects(id=_id)
-   return render_template('output_basic_form.html', _id=_id)
+   reqTweet = Tweet.objects()
+   return render_template('output_basic_form.html', _id=_id, term=reqPage[0]['filterTerm'], time=pd.to_datetime(reqPage[0]['filterTime']), 
+                           location=reqPage[0]['filterLocation'], profile=reqPage[0]['filterProfile'], tweets=reqTweet)
 
 @app.route('/descriptive/<_id>',methods=["GET", "POST"])
 def descriptive(_id):
@@ -36,3 +44,8 @@ def id():
       return redirect(url_for('basic', _id=_id))
    if reqPage[0]['pageType'] == "descriptive":
       return redirect((url_for('descriptive', _id=_id)))
+
+@app.route('/test', methods=['GET'])
+def test():
+   reqTweet = Tweet.objects().to_json()
+   return reqTweet
