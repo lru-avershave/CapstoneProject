b713accf5f80ad840784d7afe4ee69f690b0e6fc
MJM <mjmcgaffick@yahoo.com>
2020-02-24 18:35:17 -0500

    Adds a list and fixes an error with the dictionary

--- utils/LocationCounter.py
+++ utils/LocationCounter.py
@@ -1,9 +1,10 @@
 from TweetsToDB.TweetModel import *
 
 def locationCounter(reqTweet):
-       locations = reqTweet.distinct("location") 
+       locations = reqTweet.distinct("location")
+       locationTotals = [] 
        for i in locations:
-           locationTotals = {
-                            "location" : i
-                            "count" : Tweet.objects("location"=i).count()
-            }
+           locationTotals.append({
+                            "location" : i,
+                            "count" : Tweet.objects(location=i).count()
+            })
